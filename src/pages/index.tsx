e471631ae9893508f38f7fbb3b40dc37da9de0cc
chris-cybula <chris.cybula@gmail.com>
2020-10-22 20:35:22 +0100

    Add ts to props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,32 @@
 import React from "react"
 
-const App = () => {
+interface Items {
+  number: number
+  string: string
+}
 
-  const test: number = 66;
+const Component: React.FC<Items> = ({ number, string }) => {
+  return (
+    <>
+      <p>{number}</p>
+      <p>{string}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const test: number = 66
 
   const fun = () => {
     return `${test}`
   }
-  return <div>{fun()}</div>
+
+  return (
+    <>
+      <div>{fun()}</div>
+      <Component number={12 } string={"12345"} />
+    </>
+  )
 }
 
 export default App

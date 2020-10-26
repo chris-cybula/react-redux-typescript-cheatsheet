8c88f8556e8a5067d05d85085435a8d0111eae64
chris-cybula <chris.cybula@gmail.com>
2020-10-26 20:55:33 +0000

    Add ts on events

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, { useState, useEffect } from "react"
 
 interface Items {
   number: number
@@ -7,13 +7,14 @@ interface Items {
 
 const Component: React.FC<Items> = ({ number, string }) => {
   interface User {
-    name: string;
+    name: string
   }
 
-  const [user, setUser] = useState<User>({name: 'Chris!'})
+  const [user, setUser] = useState<User>({ name: "Chris!" })
 
-  const funTest = () => {
-    setUser({name: 'something'})
+  const funTest = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>{
+    setUser({ name: "something" })
+    alert(event.currentTarget)
   }
 
   return (
@@ -24,6 +25,19 @@ const Component: React.FC<Items> = ({ number, string }) => {
       <button onClick={funTest}>button</button>
     </>
   )
+}
+
+const Component2 = () => {
+
+  
+
+  let myAdd = (x:number, y:number)  => {
+    return x + y;
+  };
+
+  myAdd(1, 12)
+
+  return <p>asdsdf</p>
 }
 
 const App = () => {
@@ -37,8 +51,28 @@ const App = () => {
     <>
       <div>{fun()}</div>
       <Component number={12} string={"12345"} />
+      <Component2 />
     </>
   )
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
